07d18d4c8785fc0df12e5e0ab9a79a162aaab7f2
nithyakalyani <101079997+21nithya@users.noreply.github.com>
2023-09-06 11:39:36 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponet } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Dialog, DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud';
+  displayedColumns: string[] = ['firstName','lastName','email','dob','gender','education','company','experience','package','id','action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private _dialog: MatDialog, private _empService:EmployeeService) {}
+
+  ngOnInit(): void{
+    this.getEmployeeList();
+  }
+
+  openAddEdit(){
+    const dialogRef = this._dialog.open(EmpAddEditComponet);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if(val) {
+          this.getEmployeeList();
+        }
+      }
+    })
+  }
+  getEmployeeList(){
+    this._empService.getEmployeeList().subscribe({
+      next:(res) =>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator
+      },
+      error: (err) =>{
+        console.log(err)
+      }
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  deleteEmployee(id:number){
+    this._empService.deleteEmployee(id).subscribe({
+      next:(res) =>{
+        alert('employee deleted');
+        this.getEmployeeList();
+      },
+      error: (err)=>{
+        console.log(err)
+      }
+    })
+  }
+
+  openEditFrom(data: any) {
+    this._dialog.open(EmpAddEditComponet,{
+      data,
+    });
+  }
 }
---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+    selector: 'app-emp-add-edit',
+    templateUrl: './emp-add-edit.component.html',
+    styleUrls: ['./emp-add-edit.component.scss'],
+})
+
+export class EmpAddEditComponet {
+    education: string[] = [
+        'matric',
+        'Diploma',
+        'Graduate',
+        'Post Graduate'
+    ];
+
+    empForm: FormGroup;
+
+    constructor (private _fb:FormBuilder, private _empService:EmployeeService, private _dialogRef:MatDialogRef<EmpAddEditComponet>){
+        this.empForm=this._fb.group({
+            firstName:"",
+            lastName:'',
+            email:'',
+            dob:'',
+            gender:'',
+            education:'',
+            company:'',
+            experience:'',
+            package:''
+        })
+    }
+    onFormSubmit(){
+        if(this.empForm.valid) {
+            this._empService.addEmployee(this.empForm.value).subscribe({
+                next: (val:any) => {
+                    alert('employee add successfully')
+                    this._dialogRef.close(true);
+                },
+                error: (err: any) => {
+                    console.error(err);
+                },
+            });
+        }
+    }
+}
